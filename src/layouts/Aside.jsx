4de3af0126181c966b5b46f24642f587e5e7a328
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2024-02-22 16:01:01 +0700

    scrollbar aside

--- src/layouts/Aside.jsx
+++ src/layouts/Aside.jsx
@@ -3,25 +3,29 @@ import Logo from "../components/Logo";
 
 export default function Aside({ zIndex, isOpen }) {
   return (
-    <div
-      className={`${zIndex} ${
-        isOpen ? "block" : "  hidden lg:block"
-      } min-h-screen bg-slate-800 text-white md:w-2/12 sm:w-2/12  shadow-2xl border-r  border-slate-900 text-center pt-10 fixed  overflow-scroll focus:overscroll-auto `}
-    >
-      <Logo to={"/"} classname=" " />
-      <Main />
+    <div className="invisible">
+      <div
+        className={`${zIndex} ${
+          isOpen ? "block" : "  hidden lg:block"
+        } h-full bg-slate-800 text-white md:w-2/12 sm:w-2/12  shadow-2xl border-r  border-slate-900 text-center  fixed top-0  overflow-y-scroll visible `}
+      >
+        <div className="sticky top-0 left-0 right-0 bg-slate-800 pb-10 py-5 w-full  ">
+          <Logo to={"/"} classname="  text-center   " />
+        </div>
+        <Main />
+      </div>
     </div>
   );
 }
 
 const Main = () => {
   return (
-    <>
+    <div className="visible">
       {Array(40)
         .fill()
         .map((_, i) => (
           <p key={i}>hai</p>
         ))}
-    </>
+    </div>
   );
 };
