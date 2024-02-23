67e92e698ad7f407618a053c27b7b9f77606cd9c
isfandiar adi <141855809+isfndiar@users.noreply.github.com>
2024-02-23 08:51:22 +0700

    dropdown menu & navbar active

--- src/layouts/Aside.jsx
+++ src/layouts/Aside.jsx
@@ -1,8 +1,15 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../components/Logo";
 import * as Icon from "react-feather";
+import { useState } from "react";
 
 export default function Aside({ zIndex, isOpen }) {
+  const setIsOpen = ({ isActive, isPending }) =>
+    isActive
+      ? "bg-purple-500 px-2 py-2 rounded-md"
+      : isPending
+      ? ""
+      : "px-2 py-2";
   return (
     <div className="invisible">
       <div
@@ -13,73 +20,66 @@ export default function Aside({ zIndex, isOpen }) {
         <div className="sticky top-0 left-0 right-0 bg-slate-800 pb-10 py-5 w-full  ">
           <Logo to={"/"} classname="  text-center   " />
         </div>
-        <Main />
+        <Main setIsOpen={setIsOpen} />
       </div>
     </div>
   );
 }
 
 const Main = () => {
+  const [isClick, setIsClick] = useState(true);
   const setIsOpen = ({ isActive, isPending }) => {
     return isActive
-      ? "bg-purple-500 px-2 py-2 rounded-md"
+      ? ` bg-purple-500 px-2 py-2  flex gap-3 rounded-lg mx-7 `
       : isPending
       ? ""
-      : "px-2 py-2";
+      : "px-2 py-2 flex gap-3 rounded-lg mx-7 ";
   };
+
   return (
-    <div className="visible flex flex-col mt-5 gap-5  text-sm">
-      {/* {Array(40)
-        .fill()
-        .map((_, i) => (
-          <p key={i}>hai</p>
-        ))} */}
-      <NavLink
-        to={"/"}
-        className={`${setIsOpen} flex gap-3   rounded-lg mx-7 `}
-      >
+    <div className="flex flex-col mt-5 gap-5  text-sm">
+      <NavLink to={"/setting/profile"} className={setIsOpen}>
         <Icon.User size={20} /> Profile
       </NavLink>
-      <NavLink
-        to={"/anime"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <NavLink to={"/genre"} className={setIsOpen}>
         <Icon.File size={20} /> Genres
       </NavLink>
-      <NavLink
-        to={"/about"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
-        <Icon.FileText size={20} /> MyList <Icon.ArrowDown />
-      </NavLink>
-      <NavLink
-        to={"/return"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <div className="Dropdown-Menu">
+        <NavLink
+          onClick={() => setIsClick((isClick) => !isClick)}
+          className={
+            isClick ? setIsOpen : "px-2 py-2  flex gap-3 rounded-lg mx-7"
+          }
+        >
+          <Icon.FileText size={20} /> MyList{" "}
+          <Icon.ArrowDown
+            className={`transition-all ${isClick ? "" : "rotate-180"} `}
+          />
+        </NavLink>
+        {isClick && (
+          <div className="flex flex-col gap-2 text-xs ml-20 mt-2  me-5 h-28 text-start overflow-y-auto scroll-purple ">
+            <div>Last Watched</div>
+            <div>Watching New</div>
+            <div>Want To Watch</div>
+            <div>Complete Later</div>
+            <div>Completed</div>
+          </div>
+        )}
+      </div>
+
+      <NavLink to={"/download"} className={setIsOpen}>
         <Icon.Download size={20} /> Download
       </NavLink>
-      <NavLink
-        to={"/homee"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <NavLink to={"/favorite"} className={setIsOpen}>
         <Icon.Heart size={20} /> Favorite
       </NavLink>
-      <NavLink
-        to={"/homee"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <NavLink to={"/evaluation"} className={setIsOpen}>
         <Icon.Grid size={20} /> Global evaluation
       </NavLink>
-      <NavLink
-        to={"/homee"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <NavLink to={"/episodes"} className={setIsOpen}>
         <Icon.Calendar size={20} /> Episode Dates
       </NavLink>
-      <NavLink
-        to={"/homee"}
-        className={`${setIsOpen} flex rounded-lg mx-7 gap-3 `}
-      >
+      <NavLink to={"/setting"} className={setIsOpen}>
         <Icon.Settings size={20} /> Setting
       </NavLink>
     </div>
